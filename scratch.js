193acd198a5b62edaf4164c5960a1b09c6ffe33a
Gurijala Praneeth Reddy <praneethreddy@Gurijalas-MacBook-Air.local>
2026-05-09 10:41:50 +0530

    Cleanup: Remove internal prompts and scratch scripts from repository

--- scratch.js
+++
@@ -1,8 +0,0 @@
-const apiKey = 'c6a7976db43635102312d399b80c929afd6aaf8c98dde7a33920be22e1375167';
-fetch(`https://serpapi.com/search.json?engine=google_local&q=movie+OR+music+OR+food+restaurants+vegetarian+in+Bangalore&api_key=${apiKey}`)
-  .then(res => res.json())
-  .then(data => {
-    if (data.local_results) {
-      data.local_results.slice(0,3).forEach(e => console.log(e.title));
-    }
-  });
--- test_serp.js
+++
@@ -1,7 +0,0 @@
-const apiKey = 'c6a7976db43635102312d399b80c929afd6aaf8c98dde7a33920be22e1375167';
-fetch(`https://serpapi.com/search.json?engine=google_local&q=best+restaurant+in+Bangalore&api_key=${apiKey}`)
-  .then(res => res.json())
-  .then(data => {
-    const top = data.local_results.slice(0, 2);
-    console.log(JSON.stringify(top, null, 2));
-  });
